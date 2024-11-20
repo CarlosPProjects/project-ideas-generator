5fec1c2f0b7662df89bc0068e39c586eba655cc0
CarlosPProjects <cg9822@gmail.com>
2024-11-20 01:16:52 +0100

    Refactor global CSS styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import '@/styles/globals.css';
+
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
