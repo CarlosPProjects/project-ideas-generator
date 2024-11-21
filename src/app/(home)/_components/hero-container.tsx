f46d2c6c269ae1989d6954181bd86b0fae33f609
CarlosPProjects <cg9822@gmail.com>
2024-11-21 19:44:04 +0100

    Refactor Container component styles and update background in Home page

--- src/app/(home)/_components/hero-container.tsx
+++ src/app/(home)/_components/hero-container.tsx
@@ -14,7 +14,7 @@ const HeroContainer = () => {
       }}
       transition={{ duration: 0.5, delay: 0.3, ease: "easeIn" }}
       className="max-w-3xl text-center space-y-8 z-10">
-      <h1 className="text-7xl font-medium">Think, plan, and track <span className="text-muted-foreground">all in one place</span></h1>
+      <h1 className="text-4xl md:text-5xl lg:text-7xl font-semibold md:font-medium">Think, plan, and track <br /><span className="text-muted-foreground">all in one place</span></h1>
       <p>Efficently manage your tasks and boost productivity</p>
       <Link className={buttonVariants({ size: 'lg' })} href='/ideas'>Get free demo</Link>
     </motion.div>
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,7 +4,7 @@ import HeroContainer from "./_components/hero-container"
 
 const Home = () => {
   return (
-    <Container className="flex-1 flex justify-center items-center bg-accent mb-4 rounded-[32px] border border-border relative overflow-hidden __bg-hero">
+    <Container className="flex-1 flex justify-center items-center bg-accent mx-4 mb-4 rounded-[32px] px-4 py-8 border border-border relative overflow-hidden __bg-hero">
       <HeroContainer />
       <FloatingBox initialPosition="-100%" className="-left-8 top-24 bg-[#fff9b1]" />
       <FloatingBox initialPosition="100%" className="-right-8 bottom-24 bg-gray-100" />
