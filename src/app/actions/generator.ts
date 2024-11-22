36b6ed3fb574c3640a44d3688a5c2cdc82888f4f
CarlosPProjects <cg9822@gmail.com>
2024-11-22 20:23:26 +0100

    Add delay to idea generation function; improve user experience with asynchronous feedback

--- src/app/actions/generator.ts
+++ src/app/actions/generator.ts
@@ -1,16 +1,19 @@
 'use server'
+
+import { setTimeout } from 'timers/promises';
+
 export type State = | {
   status: "success",
   message: string,
 } | null
 
 export const generateIdeas = async (prevState: State | null, data: FormData): Promise<State> => {
-  
   const type = data.get('type')
+
+  await setTimeout(1000);
 
   return {
     status: "success",
     message: `You have selected ${type} as your project type.`
   }
-  
 }
