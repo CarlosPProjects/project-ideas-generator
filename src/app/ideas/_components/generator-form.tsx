25b5c93e43cd7e3129e4cdd9ed080e6cc7a977dd
CarlosPProjects <cg9822@gmail.com>
2024-11-22 02:37:59 +0100

    Refactor package dependencies and update component styles

---
+++ src/app/ideas/_components/generator-form.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { z } from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
+const formSchema = z.object({
+  inputText: z.string().min(2).max(50),
+})
+
+const GeneratorForm = () => {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      inputText: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="inputText"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
+
+export default GeneratorForm
--- src/app/ideas/page.tsx
+++ src/app/ideas/page.tsx
@@ -1,12 +1,18 @@
 import Container from '@/components/Container'
 import React from 'react'
 import CardProject from './_components/card-project'
+import GeneratorForm from './_components/generator-form'
 
 const Ideas = () => {
   return (
     <Container className='flex-1 flex flex-col'>
-      <div className='flex justify-center py-10'>
+      <div className='flex flex-col space-y-8 items-center py-10'>
         <h1 className='text-3xl font-medium'>Think It, Code It</h1>
+        <GeneratorForm />
+      </div>
+      <div className='text-center space-y-2'>
+        <h2 className='text-xl font-medium'>Not project ideas found</h2>
+        <p className=''>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
       </div>
       <div className='grid grid-cols-4'>
         <CardProject />
